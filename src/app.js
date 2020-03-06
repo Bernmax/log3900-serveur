650a5c1b7451735db6673780a51d02cf2d009c59
maxime.bernier <maxime.bernier@polymtl.ca>
2020-03-06 16:28:56 -0500

    Version avec des sockets

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ require('./db/db')
 
 app.use(express.json({limit: "50mb"}));
 
-var usersRouter = require('./player/player.route.js');
+let usersRouter = require("./player/player.route");
+let generalStatsRouter = require("./player/general.stats.route");
+let gameRouter = require("./game/game.route");
+
+
 app.use(usersRouter);
+app.use(generalStatsRouter);
+app.use(gameRouter);
 
 module.exports = app;
